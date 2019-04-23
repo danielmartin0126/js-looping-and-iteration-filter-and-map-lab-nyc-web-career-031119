7b120b0fdc233ed05743a07f8da914428f83c3f9
Danny Martin <danielmartin0126@gmail.com>
2019-04-23 21:40:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ function exactMatch(drivers, obj){
   return drivers.filter(driver =>
     driver[Object.keys(obj)[0]] === obj[Object.keys(obj)[0]]);
 }
+
+function exactMatchToList(drivers, obj){
+  return exactMatch(drivers, obj).map(d => d.name);
+}
