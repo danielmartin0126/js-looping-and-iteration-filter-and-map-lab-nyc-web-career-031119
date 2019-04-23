f272d41c9e5da5364f83d9df2ac4c928e53c9b6d
Danny Martin <danielmartin0126@gmail.com>
2019-04-23 21:25:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution here:
+function driversWithRevenueOver(arr, revenue) {
+  return arr.filter(function (driver) { return driver["revenue"] > revenue; });
+}
+
+function driverNamesWithRenueOver(arr,revenue) {
+  return arr.filter(function (driver) { return driver["revenue"] > revenue; });
+}
